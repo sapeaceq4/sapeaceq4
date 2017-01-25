b8fd17a29ca72262ffb44dafc2e8e91ab1b8737a
sapeaceq4 <sapeaceq4@gmail.com>
2017-01-25 14:10:12 +0530

    source files for Virtual Safari Example

---
+++ Cat.java
@@ -0,0 +1,10 @@
+package safari;
+
+public class Cat extends SingleRoamer {
+	public void speak() {
+		System.out.println("Cat speaks.");
+	}
+	public void eat() {
+		System.out.println("Cat eats.");
+	}
+}
