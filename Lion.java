892861fc5eb5499639c35984fdcfad7c93b49414
sapeaceq4 <sapeaceq4@gmail.com>
2017-01-25 14:11:07 +0530

    source files for Virtual Safari Example

---
+++ Lion.java
@@ -0,0 +1,10 @@
+package safari;
+
+public class Lion extends SingleRoamer {
+	public void speak() {
+		System.out.println("Lion speaks.");
+	}
+	public void eat() {
+		System.out.println("Lion eats.");
+	}
+}
