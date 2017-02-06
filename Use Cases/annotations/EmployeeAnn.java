d6f68d528b90664f6159afff573c8a2cd39534dd
sapeaceq4 <sapeaceq4@gmail.com>
2017-02-06 22:04:49 +0530

    Add files via upload

---
+++ Use Cases/annotations/EmployeeAnn.java
@@ -0,0 +1,11 @@
+package com.ace.annotations;
+
+@AuthorAnnotation(emailId = "tulika@sapient.com", authorName = "tulika")
+public class EmployeeAnn {
+
+	public static void main(String[] args) {
+	Class<EmployeeAnn> e = EmployeeAnn.class;
+	AuthorAnnotation aa = e.getAnnotation(AuthorAnnotation.class);
+	System.out.println(aa);
+	}
+}
---
+++ Use Cases/cloneable/Fruit.java
@@ -0,0 +1,26 @@
+package com.ace.cloneable;
+
+public class Fruit {
+	private String name;
+
+	public Fruit() {
+	}
+
+	public Fruit(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+}
