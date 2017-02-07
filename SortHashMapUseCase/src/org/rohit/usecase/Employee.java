7408cbb303455bb6fa6002e6e4d718e95b17289a
rohit <sapeaceq4@gmail.com>
2017-02-07 11:08:39 +0530

    Sort hashmap By Values

--- SortHashMapUseCase/src/org/rohit/usecase/Employee.java
+++ SortHashMapUseCase/src/org/rohit/usecase/Employee.java
@@ -16,7 +16,7 @@ public class Employee implements Comparable<Employee> {
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public String getName() {
+	public String getName() { 
 		return name;
 	}
 	public void setName(String name) {
