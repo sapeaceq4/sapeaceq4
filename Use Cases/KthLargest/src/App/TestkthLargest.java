188b850786242cc950c75f358700c041814b294f
Akash <sapeaceq4@gmail.com>
2017-02-07 14:36:28 +0530

    Kth Largest

---
+++ Use Cases/KthLargest/src/App/TestkthLargest.java
@@ -0,0 +1,9 @@
+package App;
+
+public class TestkthLargest {
+
+	public static void main(String[] args) {
+		int[] A = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+		System.out.println(kthLargest.findKthLargest(A, 9));
+	}
+}
