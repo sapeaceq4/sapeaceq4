1bebd2cf5706a300c2c67270dbde12aa68c9428e
rsi164 <rsingh162@sapient.com>
2017-02-13 16:51:51 +0530

    ExternalSort updated client

--- rsnace/src/main/java/com/sapient/ace/exercise13022017/ExternalMergeSort.java
+++ rsnace/src/main/java/com/sapient/ace/exercise13022017/ExternalMergeSort.java
@@ -1,6 +1,7 @@
 package com.sapient.ace.exercise13022017;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Ravdeep Singh on 2/13/2017. *
@@ -9,19 +10,15 @@ import java.io.File;
  */
 public class ExternalMergeSort {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length<1){
             System.out.println("Usage: file to be sorted");
             System.exit(0);
         }
         File inputFile = new File(args[0]);
-        System.out.println(inputFile.getParentFile());
-
-
-        /*File inputFile = new File(args[0]);
         FileSorter sorter = new FileSorter();
         sorter.sort(inputFile);
-*/
+
     }
 
 
