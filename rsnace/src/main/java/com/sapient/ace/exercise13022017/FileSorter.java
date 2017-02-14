3f118a4e1cfc632c981eb4db2897018c96d78114
rsi164 <rsingh162@sapient.com>
2017-02-14 11:25:13 +0530

    ExternalSort updated client

--- rsnace/src/main/java/com/sapient/ace/exercise13022017/FileSorter.java
+++ rsnace/src/main/java/com/sapient/ace/exercise13022017/FileSorter.java
@@ -4,45 +4,90 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 
 /**
  * Created by Ravdeep Singh on 2/13/2017.
  */
+
 public class FileSorter {
-    private static final int blockSize = 1000;
+//    private static final int blockSize = 1000;
 
     public File sort(File inputFile) throws IOException {
-
         File outPutFile = null;
-
-        long sizeOfInputfileInMB = inputFile.length() / 1000 * 1000;
-        int numberOfChunks = (int) (sizeOfInputfileInMB / 100) + 1;
-
-        List<File> tempFiles = readInputFileAndCreateSortedChunk(inputFile);
-
-        outPutFile = nWayMerge(tempFiles);       
-        
+        long sizeOfInputfileInKB = inputFile.length() / (1024);
+//        int sizeOfChunk = (int) (sizeOfInputfileInKB / 10) + 1;
+//        int sizeOfChunk = (int) (sizeOfInputfileInKB / 10) ;
+        System.out.println("sizeOfInputfileInKB " + sizeOfInputfileInKB + "sizeOfChunk: " + 102400);
+        List<File> tempFiles = readInputFileAndCreateSortedChunk(inputFile, 102400);
+        System.out.println(tempFiles.size());
+        outPutFile = nWayMerge(tempFiles);
         return outPutFile;
     }
 
     private File nWayMerge(List<File> tempFiles) throws IOException {
-        int numberOfTempFiles= tempFiles.size();
-        File outputFile = new File(tempFiles.get(1).getParent()+"/output.txt");
+        int numberOfTempFiles = tempFiles.size();
+        System.out.println();
+        File outputFile = new File(tempFiles.get(1).getParent() + "/output.txt");
         BufferedReader br[] = new BufferedReader[numberOfTempFiles];
         BufferedWriter outFile = new BufferedWriter(new FileWriter(outputFile));
-        for (int i = 0; i <numberOfTempFiles ; i++) {
+        PriorityQueue<String> mergerQ = new PriorityQueue<>();
+
+        for (int i = 0; i < numberOfTempFiles; i++) {
             br[i] = new BufferedReader(new FileReader(tempFiles.get(i)));
         }
-        //Read nad merge From files
+        String[] bufferStringArray = new String[numberOfTempFiles];
 
+        for (int i = 0; i < numberOfTempFiles; i++) {
+            BufferedReader reader = br[i];
+            String qString = reader.readLine().trim();
+            bufferStringArray[i] = qString;
+            mergerQ.add(qString);
+        }
+
+        while (true) {
+            boolean complete = false;
+            String headString;
+
+            headString = mergerQ.poll();
+            outFile.write(headString);
+            outFile.write("\n");
+
+            int index = findHeadPointer(bufferStringArray, headString);
+
+            String line;
+            if ((line = br[index].readLine()) != null) {
+                mergerQ.add(line.trim());
+                bufferStringArray[index] = line.trim();
+            }
+            for (int i = 0; i < numberOfTempFiles; i++) {
+                if (br[i].readLine() == null) {
+                    complete = true;
+                }
+            }
+
+            if (complete)
+                break;
+
+        }
         return outputFile;
+    }
+
+    private int findHeadPointer(String[] bufferStringArray, String headString) {
+
+        for (int i = 0; i < bufferStringArray.length; i++) {
+            if (bufferStringArray[i].equalsIgnoreCase(headString))
+                return i;
+
+        }
+        return -1;
     }
 
 
     public static File sortandcreatetemp(List<String> tmplist) throws IOException {
         Collections.sort(tmplist);
-        File newtmpfile = File.createTempFile("sortInBatch", "flatfile");
-        newtmpfile.deleteOnExit();
+        File newtmpfile = File.createTempFile("temp", ".txt");
+//        newtmpfile.deleteOnExit();
         BufferedWriter fbw = new BufferedWriter(new FileWriter(newtmpfile));
         try {
             for (String r : tmplist) {
@@ -55,22 +100,23 @@ public class FileSorter {
         return newtmpfile;
     }
 
-    private List<File> readInputFileAndCreateSortedChunk(File inputFile) throws IOException {
+    private List<File> readInputFileAndCreateSortedChunk(File inputFile, int size) throws IOException {
         List<File> files = new ArrayList<>();
         BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
-        long blocksize = blockSize;
+        long blocksize = size;
         try {
             List<String> tmplist = new ArrayList<>();
             String line = "";
             try {
                 long currentblocksize = 0;
-                while ((currentblocksize < blocksize) && ((line = bufferedReader.readLine()) != null)){
-                    while (line != null) {
-                        tmplist.add(line);
-                        currentblocksize += line.length();
+                while ((currentblocksize < blocksize) && ((line = bufferedReader.readLine()) != null)) {
+                    tmplist.add(line);
+                    currentblocksize += line.length();
+                    if (currentblocksize >= blocksize) {
+                        files.add(sortandcreatetemp(tmplist));
+                        tmplist.clear();
+                        currentblocksize = 0;
                     }
-                    files.add(sortandcreatetemp(tmplist));
-                    tmplist.clear();
                 }
             } catch (EOFException oef) {
                 if (tmplist.size() > 0) {
