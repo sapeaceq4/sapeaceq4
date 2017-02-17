32c9687c1ec76bd146476180ff9f3b61499b1031
sapeaceq4@gmail.com <pwd12345>
2017-02-17 11:34:51 +0530

    usecase3

---
+++ UseCases/useCase3/Consumer.java
@@ -0,0 +1,23 @@
+package data.session.usecases;
+
+import java.util.concurrent.BlockingQueue;
+
+public class Consumer implements Runnable {
+	
+	protected BlockingQueue<String> queue = null;
+
+	public Consumer(BlockingQueue<String> queue) {
+		this.queue = queue;
+	}
+
+	public void run() {
+		try {
+			System.out.println(queue.take());
+			System.out.println(queue.take());
+			System.out.println(queue.take());
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
