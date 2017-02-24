070bdd807c8f2020915e6f3d617b2b2f89ecacb8
Akash <sapeaceq4@gmail.com>
2017-02-24 17:06:04 +0530

    test

---
+++ Use Cases/GenericObjectPool/src/com/iluwatar/object/pool/OliphauntPool.java
@@ -0,0 +1,16 @@
+/**
+ */
+package com.iluwatar.object.pool;
+
+/**
+ * 
+ * Oliphaunt object pool
+ *
+ */
+public class OliphauntPool extends ObjectPool<Oliphaunt> {
+
+	@Override
+	protected Oliphaunt create() {
+		return new Oliphaunt();
+	}
+}
