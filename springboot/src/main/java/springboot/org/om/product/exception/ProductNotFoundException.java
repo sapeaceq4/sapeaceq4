fc81a5fddf8f8ad190a73e11d31af9822dca9644
spaeaceq4@gmail.com <pwd12345>
2017-02-28 09:38:08 +0530

    added exception

--- springboot/src/main/java/springboot/org/om/product/exception/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package springboot.org.om.product.exception;
-
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
-
-public class ProductNotFoundException extends ExceptionHandlerExceptionResolver
-{
-
-}
